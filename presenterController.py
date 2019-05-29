f8828b7b69d790ef7e7e063a4ea472a8e246342e
SmejkalJakub <smejkal.jakub99@gmail.com>
2019-05-29 10:47:05 +0200

    Update presenterController.py

--- presenterController.py
+++ presenterController.py
@@ -6,40 +6,41 @@ keyboard = Controller()
 #define callback
 def on_message(client, userdata, message):
     time.sleep(1)
-    if(str(message.payload.decode("utf-8")) == "next"):
+    message = str(message.payload.decode("utf-8")
+    if(message == "next"):
         print("next slide")
         keyboard.press(Key.right)
         keyboard.release(Key.right)
-    elif(str(message.payload.decode("utf-8")) == "prev"):
+    elif(message == "prev"):
         print("prev slide")
         keyboard.press(Key.left)
         keyboard.release(Key.left)
-    elif(str(message.payload.decode("utf-8")) == "start"):
+    elif(message == "start"):
         print("start presentation")
         keyboard.press(Key.f5)
         keyboard.release(Key.f5)
-    elif(str(message.payload.decode("utf-8")) == "tab"):
+    elif(message == "tab"):
         print("tab button")
         keyboard.press(Key.alt)
         keyboard.press(Key.tab)
         keyboard.release(Key.tab)
-    elif(str(message.payload.decode("utf-8")) == "enter"):
+    elif(message == "enter"):
         print("enter button")
         keyboard.release(Key.alt)
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
-    elif(str(message.payload.decode("utf-8")) == "esc"):
+    elif(message == "esc"):
         print("esc button")
         keyboard.release(Key.alt)
         keyboard.press(Key.esc)
         keyboard.release(Key.esc)
-    elif(str(message.payload.decode("utf-8")) == "end"):
+    elif(message == "end"):
         print("end button")
         keyboard.press(Key.alt)
         keyboard.press(Key.f4)
         keyboard.release(Key.f4)
         keyboard.release(Key.alt)
-    elif(str(message.payload.decode("utf-8")) == "space"):
+    elif(message == "space"):
         print("space button")
         keyboard.release(Key.alt)
         keyboard.press(Key.space)
@@ -56,4 +57,3 @@ print("subscribing ")
 client.subscribe("presenter/action")#subscribe
 while 1:
     time.sleep(1)
-
